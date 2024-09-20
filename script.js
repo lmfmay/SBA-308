c60b72122801d574b2fc8be2ba535c230df6a5d2
lmfmay <176079985+lmfmay@users.noreply.github.com>
2024-09-19 23:55:41 -0700

    Cleaned up comments

--- script.js
+++ script.js
@@ -90,17 +90,17 @@ function getLearnerData(course, ag, submissions) {
     }
   } catch (error) {console.log(error)}
 
-//Transform learner submission data to learner objects.
+//Transform learner submission data to learnerData objects.
   for (let sub of submissions) {  
     if (!allLearners.some(el => el.id == sub.learner_id)){ //if learner doesn't exist
-        learnerData.id = sub.learner_id;
-        learnerData[sub.assignment_id]=sub.submission.score;
-        allLearners.push(learnerData);
-        learnerData = {};
-      } else{ //if learner exists
+        learnerData.id = sub.learner_id; //create id key-value pair
+        learnerData[sub.assignment_id]=sub.submission.score; //create assignment score key-value pair
+        allLearners.push(learnerData); // push id and assignment score to main array
+        learnerData = {}; //clear for next new learner
+      } else{ 
         allLearners.forEach((learner) => { 
-          if (learner.id == sub.learner_id){
-            learner[sub.assignment_id] = sub.submission.score;
+          if (learner.id == sub.learner_id){ //if learner id key-value already exsists
+            learner[sub.assignment_id] = sub.submission.score; //create assignment score key-value in learner object
           }
         }); 
       }
@@ -109,55 +109,54 @@ function getLearnerData(course, ag, submissions) {
     let submissionsDateAll = []; //assignments and submission dates of all learners
     let submissionsDate = {}; //assignments and submission dates of each learner
     allLearners.forEach((learner) => { 
-      submissions.forEach(submission => {
-        if (learner.id == submission.learner_id){
-          submissionsDate.id = learner.id;
-          submissionsDate[submission.assignment_id] = submission.submission.submitted_at;
+      submissions.forEach(submission => { //iterates through learner submissions database
+        if (learner.id == submission.learner_id){ //if learner id matches submission
+          submissionsDate.id = learner.id; // create id key-value in learner submission object
+          submissionsDate[submission.assignment_id] = submission.submission.submitted_at; //create assignment submission date key-value in learner submission object 
         }
       })
-      submissionsDateAll.push(submissionsDate);
-      submissionsDate = {}
+      submissionsDateAll.push(submissionsDate); //Push submission data to array
+      submissionsDate = {} //clear for next learner
     });
         
 
 // for each assignment, get due date
-    let dueDate = {};
+
+    let dueDate = {}; //due dates of all assignments
+
+// get currentDate to remove assignments that are not yet due.
     const date = new Date()
     let day = date.getDate();
     let month = date.getMonth() + 1;
     let year = date.getFullYear();
 
-    // This arrangement can be altered based on how we want the date's format to appear.
     let currentDate = `${year}-${month}-${day}`;
    
-    ag.assignments.forEach(element => {
-      dueDate[element.id] = element.due_at;
-      if (element.due_at > currentDate){
-        allLearners.forEach(learner => {
-          delete learner[element.id];
+    ag.assignments.forEach(element => { //for each assignment in assignment group
+      dueDate[element.id] = element.due_at; //create assignment duedate key-value pair
+      if (element.due_at > currentDate){ // if assignment not yet due
+        allLearners.forEach(learner => { 
+          delete learner[element.id]; //remove assignment property in main array for each learner
         });
       }
     });
     
 
 // for each assignment, get points possible
-    let pointsPossible = {};
+    let pointsPossible = {}; // possible points of all assignements
     ag.assignments.forEach(element => {
-      if (element.due_at < currentDate){
-        pointsPossible[element.id] = element.points_possible
+      if (element.due_at < currentDate){ //if assignment is already due
+        pointsPossible[element.id] = element.points_possible //create assignment points possible key-value pair
       }
     });
 
-// for each learner, compare due date and submission date
-// submission > due = penalty
-// submission <= due = nothing happens
-
-for (let i = 1; i < Object.keys(dueDate).length; i++) {
-  allLearners.forEach((learner) => {
-    submissionsDateAll.forEach(submission => {      
-        if (submission.id == learner.id && submission[i] > dueDate[i]){
-          learner[i] -= (pointsPossible[i]*0.1);
-        }         
+// for each learner, compare due date and submission date. if submission > due = 10% penalty
+for (let i = 1; i < Object.keys(dueDate).length; i++) { //for each assignment 
+  allLearners.forEach((learner) => { // pull learner id and scores for each learner 
+    submissionsDateAll.forEach(submission => { // pull learner id and submission dates for each learner 
+        if (submission.id == learner.id && submission[i] > dueDate[i]){ //if learner ID matches AND assignment submission date is later than due date 
+          learner[i] -= (pointsPossible[i]*0.1); //deduct 10% of pointsPossible off learner score
+        }
       });
     })
 };
@@ -166,33 +165,34 @@ for (let i = 1; i < Object.keys(dueDate).length; i++) {
 // get weighted avg for each learner
 // for each learner, total scores AND possible scores for all assignments to get weighted average score.
     // e.g. a learner with 50/100 on one assignment and 190/200 on another would have a weighted average score of 240/300 = 80%.
-    let learnerSum = 0;
-    let possibleSum = 0;
-    allLearners.forEach((learner) => {
-      for (let points in pointsPossible){
-        possibleSum += pointsPossible[points];
+    let learnerSum = 0; //allocate memory for sum of learner scores
+    let possibleSum = 0; //allocate memory for possible sum of all assignments
+    allLearners.forEach((learner) => {//for each learner
+      for (let points in pointsPossible){ //pull points possible for each assignment
+        possibleSum += pointsPossible[points]; //add points to sum of possible points for all assignments
       }
-      for (let i = 1; i < Object.keys(learner).length; i++) {
-        learnerSum += learner[i];
+      for (let i = 1; i < Object.keys(learner).length; i++) { //for each assignment
+        learnerSum += learner[i]; //add learner assignment score to sum of learner scores
       } 
-      learner.avg = learnerSum/possibleSum;
-      learnerSum = 0;
-      possibleSum = 0;
+      learner.avg = learnerSum/possibleSum; //get weighted average
+      learnerSum = 0; //clear for next learner
+      possibleSum = 0; //clear for next learner
       })
     
 // get ratio of learner scores/possible_points
-for (let i = 1; i < Object.keys(dueDate).length; i++) {
-  allLearners.forEach(learner => {
-    learner[i] /= pointsPossible[i]
-});
+
+  allLearners.forEach(learner => { //for each learner
+    for (let i = 1; i <= Object.keys(learner).length-2; i++) { //for each assignment (length-2 to remove id and avg)
+      learner[i] /= pointsPossible[i] //replace score with ratio
+    }
+  });
+
+//return main array  
+  return allLearners; 
 }
 
-return allLearners;
-}
 
-
-
-//FUNCTIONS
+//HELPER FUNCTIONS
 
 
 
